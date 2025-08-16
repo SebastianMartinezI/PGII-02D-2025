fbb8304f669b4da34352afdd2eff0451490f2b97
SebastianMartinezI <134235768+SebastianMartinezI@users.noreply.github.com>
2025-08-15 20:07:13 -0500

    Delete Cafeteria directory

--- Cafeteria/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module co.edu.uniquindio.poo.cafeteria {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens co.edu.uniquindio.poo.cafeteria to javafx.fxml;
-    exports co.edu.uniquindio.poo.cafeteria;
-}
